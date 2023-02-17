192e8e25a7002458abc7c1883760dacda2d7f620
Asaf <46109899+asafravid@users.noreply.github.com>
2023-02-17 11:52:05 +0200

    Parameterize

--- memconsume.c
+++ memconsume.c
@@ -7,18 +7,32 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("asafravid");
 MODULE_DESCRIPTION("A Kernel Module to Intentionally Consume Memory");
 
-#define NUM_PTRS 1024*1024
-#define ALLOC_SIZE 4096 // Bytes
-static void* ptrs[NUM_PTRS] = {0};
+#define NUM_PTRS 1024
+#define ALLOC_SIZE 4096*1024 // Bytes
+
+int num_ptrs = NUM_PTRS;
+int alloc_size = ALLOC_SIZE;
+
+module_param(num_ptrs, int, 0);
+module_param(alloc_size, int, 0);
+
+static char** ptrs;
 
 // TODO: ASAFR: Communicate with the kernel module somehow to consume more and more memory
 
 static int __init memconsume_init(void)
 {
     int i;
-    for (i = 0; i < NUM_PTRS; i++) {
-        ptrs[i] = kmalloc(ALLOC_SIZE, GFP_KERNEL);
-        printk(KERN_INFO "Allocated %d Bytes! Iteration %d, ptrs[%d]=%p\n", ALLOC_SIZE, i, i, ptrs[i]);
+
+    printk("Module Init: num_ptrs = %d, alloc_size = %d\n", num_ptrs, alloc_size);
+
+    ptrs = (char**)kmalloc(num_ptrs*sizeof(char*), GFP_KERNEL);
+
+    printk(KERN_INFO "Allocated %d (char*)! ptrs=%p\n", num_ptrs, (void*)ptrs);
+
+    for (i = 0; i < num_ptrs; i++) {
+        ptrs[i] = kmalloc(alloc_size, GFP_KERNEL);
+        printk(KERN_INFO "Allocated %d Bytes! ptrs[%d]=%p\n", alloc_size, i, ptrs[i]);
     }
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
@@ -27,10 +41,12 @@ static void __exit memconsume_cleanup(void)
 {
     int i;
     printk(KERN_INFO "Cleaning up module.\n");
-    for (i = 0; i < NUM_PTRS; i++) {
+    for (i = 0; i < num_ptrs; i++) {
         kfree(ptrs[i]);
-        printk(KERN_INFO "Freed %d Bytes! Iteration %d, ptrs[%d]=%p\n", ALLOC_SIZE, i, i, ptrs[i]);
+        printk(KERN_INFO "Freed %d Bytes! ptrs[%d]=%p\n", alloc_size, i, ptrs[i]);
     }
+    kfree(ptrs);
+    printk(KERN_INFO "Freed %d (char*)! ptrs=%p\n", num_ptrs, ptrs);
 }
 
 module_init(memconsume_init);
